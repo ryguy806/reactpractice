975cee32a208d9382f23764ed42ec28af75df674
ryguy806 <L1ghtSab3r11!!>
2020-06-26 11:50:43 -0700

    Practice with Dependency Arrays

--- src/index.js
+++ src/index.js
@@ -2,29 +2,30 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function GitHubUser({login}) {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${login}`)
+            .then(res => res.json())
+            .then(setData)
+            .catch(console.error);
+    }, [login])
+
+    if(data) {
+        return (
+            <div>
+                <h1>{data.login}</h1>
+                <img src={data.avatar_url} width={100} alt="Colliding Stars!"/>
+            </div>
+        );
+    }
+    return null;
+
+}
+
 function App() {
 
-    const [val, setVal] = useState("");
-    const [val2, setVal2] = useState("");
-
-    useEffect(() => {
-        console.log(`field 1: ${val}`);
-    }, [val]);
-    useEffect(() => {
-        console.log(`field 2: ${val2}`);
-    }, [val2]);
-
-    return (
-        <>
-            <label> Favorite Phrase:
-                <input value={val} onChange={e => setVal(e.target.value)}/>
-            </label>
-            <br/>
-            <label> Second Favorite Phrase:
-                <input value={val2} onChange={e => setVal2(e.target.value)}/>
-            </label>
-        </>
-    )
+    return <GitHubUser login="moonhighway"/>
 }
 
 ReactDOM.render(
