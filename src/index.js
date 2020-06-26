7f67688afa983506025a5b14387b15dec9cd2168
ryguy806 <L1ghtSab3r11!!>
2020-06-26 11:05:46 -0700

    Practice with managaing multiple useStates

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
-    
+
+    const [year, setYear] = useState(2020);
+    const [manager, setManager] = useState("Alex");
     const [status, setStatus] = useState("Open");
+
     return (
-        <div>
-            <h1>Status: {status}</h1>
-            <button onClick={() => setStatus("Open")}>Open</button>
-            <button onClick={() => setStatus("Back In 5")}>Break</button>
-            <button onClick={() => setStatus("Closed")}>Closed</button>
-        </div>
+        <>
+            <div>
+                <h1>Year: {year}</h1>
+                <button onClick={() => setYear(year+1)}>Next Year</button>
+            </div>
+            <div>
+                <h1>Manager: {manager}</h1>
+                <button onClick={() => setManager("Ryan")}>New Manager</button>
+                <button onClick={() => setManager("Sam")}>Real Manager</button>
+            </div>
+            <div>
+                <h1>Status: {status}</h1>
+                <button onClick={() => setStatus("Open")}>Open</button>
+                <button onClick={() => setStatus("Back In 5")}>Break</button>
+                <button onClick={() => setStatus("Closed")}>Closed</button>
+            </div>
+        </>
     )
 }
 
